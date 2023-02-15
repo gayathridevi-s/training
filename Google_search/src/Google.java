278376f0ea0f0ca347c8a88a5ae0203b15e63aff
gaya-pt7159 <gaya-pt7159@192.168.0.102>
2023-02-15 11:04:41 +0530

    modify abstract class code

--- Google_search/src/Google.java
+++ Google_search/src/Google.java
@@ -1,8 +1,8 @@
 
-public abstract class Google {
-public void display() {
+public abstract class Google {						//abstract class
+public void display() {													//abstract method
 	System.out.println("welcome to google search!");
 
 }
-public abstract void search();
+public abstract void search();      //non abstract method
 }
