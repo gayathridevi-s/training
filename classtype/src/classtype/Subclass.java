2ba773bbd6fb8cbd2ff306dc56adf5c02bb3dbac
gaya-pt7159 <gaya-pt7159@192.168.0.102>
2023-02-15 13:17:45 +0530

    modified

--- classtype/src/classtype/Subclass.java
+++ classtype/src/classtype/Subclass.java
@@ -2,10 +2,10 @@ package classtype;
 
 public class Subclass extends Superclass {
 	public static void print() {
-		System.out.println("this is subclass");
+		System.out.println("this is subclass");					//method hiding
 	}
 	public void display() {
-		System.out.println("dyanamic binding in subclass");
+		System.out.println("dyanamic binding in subclass");				//method overriding
 	
 	}
 	public void supercall() {
