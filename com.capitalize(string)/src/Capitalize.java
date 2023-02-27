91d3cea1692eb8d6a4fac6bb98326b444b2282a4
gaya-pt7159 <gaya-pt7159@192.168.43.136>
2023-02-27 21:50:13 +0530

    toggle method and capitalize method

--- com.capitalize(string)/src/Capitalize.java
+++ com.capitalize(string)/src/Capitalize.java
@@ -1,25 +1,41 @@
-class Capital{
+class Capital {
 	String input;
-	Capital(String input){
-		this.input=input;
+
+	Capital(String input) {
+		this.input = input;
 	}
+
 	String capitalizeString() {
-		String[] words=input.split(" ");
-		String capitalString="";
-		for(String word:words) {
-			String firstLtter=word.substring(0,1);
-			String remainingWord=word.substring(1);
-			
-			capitalString+=firstLtter.toUpperCase()+remainingWord+" ";
+		String[] words = input.split(" ");
+		String capitalString = "";
+		for (String word : words) {
+			String firstLtter = word.substring(0, 1);
+			String remainingWord = word.substring(1);
+
+			capitalString += firstLtter.toUpperCase() + remainingWord + " ";
 		}
-		return capitalString ;
-		
+		return capitalString;
+
+	}
+
+	String toggle() {
+		String[] words = input.split(" ");
+		String toggleString = "";
+		for (String word : words) {
+			String first = word.substring(0, 1);
+			String remainingWord = word.substring(1);
+
+			toggleString += first.toLowerCase() + remainingWord.toUpperCase() + " ";
+		}
+		return toggleString;
 	}
 }
+
 public class Capitalize {
 	public static void main(String[] args) {
-		Capital line=new Capital("i am hi");
+		Capital line = new Capital("i am hi");
 		System.out.println(line.capitalizeString());
+		System.out.println(line.toggle());
 	}
 
 }
