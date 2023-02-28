eca046ff8703ce1f37585f5e69d7fa8a9649e04b
gaya-pt7159 <gaya-pt7159@192.168.43.136>
2023-03-01 01:19:36 +0530

    tried string examples

--- com.capitalize(string)/src/Capitalize.java
+++ com.capitalize(string)/src/Capitalize.java
@@ -18,24 +18,37 @@ class Capital {
 
 	}
 
-	String toggle() {
+	StringBuilder toggle() {
 		String[] words = input.split(" ");
-		String toggleString = "";
+
+		StringBuilder toggleString = new StringBuilder();
 		for (String word : words) {
 			String first = word.substring(0, 1);
 			String remainingWord = word.substring(1);
 
-			toggleString += first.toLowerCase() + remainingWord.toUpperCase() + " ";
+			toggleString.append(first.toLowerCase()).append(remainingWord.toUpperCase()).append(" ");
 		}
 		return toggleString;
+	}
+
+	String removeAllChar(char x) {
+
+		return input.replace(String.valueOf(x), ""); // java does not allow empty characters,
+	} // "" is empty string.String.valueOf(x) converts character to string
+
+	String removeSingleChar(int location) {
+		return input.substring(0, location) + input.substring(location + 1);
+
 	}
 }
 
 public class Capitalize {
 	public static void main(String[] args) {
-		Capital line = new Capital("i am hi");
+		Capital line = new Capital("i am gayathri");
 		System.out.println(line.capitalizeString());
 		System.out.println(line.toggle());
+		System.out.println(line.removeAllChar('a'));
+		System.out.println(line.removeSingleChar(7));
 	}
 
 }
--- com.string/src/com/string/Strings.java
+++ com.string/src/com/string/Strings.java
@@ -1,7 +1,2 @@
 package com.string;
 
-public class Strings {
-	public static void main(Strings[] args) {
-		System.out.println("hello");
-	}
-}
